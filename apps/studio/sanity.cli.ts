c56328377bb0d94489714a26c8e1d81ab34053af
Eigil Nikolajsen <eigi0088@gmail.com>
2025-02-13 13:02:32 +0100

    update: Docs

--- apps/studio/sanity.cli.ts
+++ apps/studio/sanity.cli.ts
@@ -10,5 +10,5 @@ export default defineCliConfig({
 	 * Learn more at https://www.sanity.io/docs/cli#auto-updates
 	 */
 	autoUpdates: true,
-	// studioHost: "novo-holdings-annual-report-2024",
+	studioHost: "novo-holdings-annual-report-2024",
 });
