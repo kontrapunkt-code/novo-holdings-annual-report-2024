65ce1e855dcbbde3336397545ae421486a164fcd
Eigil Nikolajsen <eigi0088@gmail.com>
2025-02-19 15:46:50 +0100

    feature: Layout

--- apps/studio/typegen/sanity.types.ts
+++ apps/studio/typegen/sanity.types.ts
@@ -103,55 +103,11 @@ export type HighlightsModule = {
 
 export type SideBySideModule = {
 	_type: "sideBySideModule";
+	caption?: string;
 	title?: string;
-	leftContent?: Array<{
-		children?: Array<{
-			marks?: Array<string>;
-			text?: string;
-			_type: "span";
-			_key: string;
-		}>;
-		style?: "normal" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "blockquote";
-		listItem?: "bullet" | "number";
-		markDefs?: Array<{
-			href?: string;
-			_type: "link";
-			_key: string;
-		}>;
-		level?: number;
-		_type: "block";
-		_key: string;
-	}>;
-	rightContent?: Array<{
-		children?: Array<{
-			marks?: Array<string>;
-			text?: string;
-			_type: "span";
-			_key: string;
-		}>;
-		style?: "normal" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "blockquote";
-		listItem?: "bullet" | "number";
-		markDefs?: Array<{
-			href?: string;
-			_type: "link";
-			_key: string;
-		}>;
-		level?: number;
-		_type: "block";
-		_key: string;
-	}>;
-	image?: {
-		asset?: {
-			_ref: string;
-			_type: "reference";
-			_weak?: boolean;
-			[internalGroqTypeReferenceTo]?: "sanity.imageAsset";
-		};
-		hotspot?: SanityImageHotspot;
-		crop?: SanityImageCrop;
-		_type: "image";
-	};
-	imagePosition?: "left" | "right";
+	description?: string;
+	buttonText?: string;
+	link?: string;
 };
 
 export type ArticleFigureModule = {
@@ -173,7 +129,10 @@ export type ArticleFigureModule = {
 
 export type ArticleGalleryModule = {
 	_type: "articleGalleryModule";
-	title?: string;
+	caption?: string;
+	description?: string;
+	buttonText?: string;
+	link?: string;
 	images?: Array<{
 		asset?: {
 			_ref: string;
@@ -183,8 +142,6 @@ export type ArticleGalleryModule = {
 		};
 		hotspot?: SanityImageHotspot;
 		crop?: SanityImageCrop;
-		caption?: string;
-		alt?: string;
 		_type: "image";
 		_key: string;
 	}>;
@@ -486,7 +443,7 @@ export type AllSanitySchemaTypes =
 export declare const internalGroqTypeReferenceTo: unique symbol;
 // Source: ./lib/queries.ts
 // Variable: PAGES_QUERY
-// Query: *[_type == "page" || _type == "case"] {		_type,		title,		slug,		project,		startDate,		endDate,		heroImage,		modules[] {			_type == "atAGlanceModule" => {				...,			},			_type == "articleTextModule" => {				...,			},			_type == "articleQuoteModule" => {				...,			},			_type == "articleStatsModule" => {				...,			},			_type == "articleGalleryModule" => {				...,			},			_type == "articleFigureModule" => {				...,			},			_type == "sideBySideModule" => {				...,			},			_type == "highlightsModule" => {				...,			},			_type == "newsModule" => {				...,				"videoUrl": video.asset -> url,			},			_type == "caseHighlightsModule" => {				...,				cases[] -> {					title,					slug,					project,					startDate,					endDate,					heroImage,				},			},			_type == "articleHeroModule" => {				...,			},		}	}
+// Query: *[_type == "page" || _type == "case"] {		_type,		title,		slug,		project,		startDate,		endDate,		heroImage,		modules[] {			_type == "atAGlanceModule" => {				...,			},			_type == "articleTextModule" => {				...,			},			_type == "articleQuoteModule" => {				...,			},			_type == "articleStatsModule" => {				...,			},			_type == "articleGalleryModule" => {				...,			},			_type == "articleFigureModule" => {				...,			},			_type == "sideBySideModule" => {  				...,			},			_type == "highlightsModule" => {				...,			},			_type == "newsModule" => {				...,				"videoUrl": video.asset -> url,			},			_type == "caseHighlightsModule" => {				...,				cases[] -> {					title,					slug,					project,					startDate,					endDate,					heroImage,				},			},			_type == "articleHeroModule" => {				...,			},		}	}
 export type PAGES_QUERYResult = Array<
 	| {
 			_type: "case";
@@ -528,7 +485,10 @@ export type PAGES_QUERYResult = Array<
 				| {
 						_key: string;
 						_type: "articleGalleryModule";
-						title?: string;
+						caption?: string;
+						description?: string;
+						buttonText?: string;
+						link?: string;
 						images?: Array<{
 							asset?: {
 								_ref: string;
@@ -538,8 +498,6 @@ export type PAGES_QUERYResult = Array<
 							};
 							hotspot?: SanityImageHotspot;
 							crop?: SanityImageCrop;
-							caption?: string;
-							alt?: string;
 							_type: "image";
 							_key: string;
 						}>;
@@ -672,71 +630,11 @@ export type PAGES_QUERYResult = Array<
 				| {
 						_key: string;
 						_type: "sideBySideModule";
+						caption?: string;
 						title?: string;
-						leftContent?: Array<{
-							children?: Array<{
-								marks?: Array<string>;
-								text?: string;
-								_type: "span";
-								_key: string;
-							}>;
-							style?:
-								| "blockquote"
-								| "h1"
-								| "h2"
-								| "h3"
-								| "h4"
-								| "h5"
-								| "h6"
-								| "normal";
-							listItem?: "bullet" | "number";
-							markDefs?: Array<{
-								href?: string;
-								_type: "link";
-								_key: string;
-							}>;
-							level?: number;
-							_type: "block";
-							_key: string;
-						}>;
-						rightContent?: Array<{
-							children?: Array<{
-								marks?: Array<string>;
-								text?: string;
-								_type: "span";
-								_key: string;
-							}>;
-							style?:
-								| "blockquote"
-								| "h1"
-								| "h2"
-								| "h3"
-								| "h4"
-								| "h5"
-								| "h6"
-								| "normal";
-							listItem?: "bullet" | "number";
-							markDefs?: Array<{
-								href?: string;
-								_type: "link";
-								_key: string;
-							}>;
-							level?: number;
-							_type: "block";
-							_key: string;
-						}>;
-						image?: {
-							asset?: {
-								_ref: string;
-								_type: "reference";
-								_weak?: boolean;
-								[internalGroqTypeReferenceTo]?: "sanity.imageAsset";
-							};
-							hotspot?: SanityImageHotspot;
-							crop?: SanityImageCrop;
-							_type: "image";
-						};
-						imagePosition?: "left" | "right";
+						description?: string;
+						buttonText?: string;
+						link?: string;
 				  }
 			> | null;
 	  }
@@ -769,7 +667,10 @@ export type PAGES_QUERYResult = Array<
 				| {
 						_key: string;
 						_type: "articleGalleryModule";
-						title?: string;
+						caption?: string;
+						description?: string;
+						buttonText?: string;
+						link?: string;
 						images?: Array<{
 							asset?: {
 								_ref: string;
@@ -779,8 +680,6 @@ export type PAGES_QUERYResult = Array<
 							};
 							hotspot?: SanityImageHotspot;
 							crop?: SanityImageCrop;
-							caption?: string;
-							alt?: string;
 							_type: "image";
 							_key: string;
 						}>;
@@ -913,71 +812,11 @@ export type PAGES_QUERYResult = Array<
 				| {
 						_key: string;
 						_type: "sideBySideModule";
+						caption?: string;
 						title?: string;
-						leftContent?: Array<{
-							children?: Array<{
-								marks?: Array<string>;
-								text?: string;
-								_type: "span";
-								_key: string;
-							}>;
-							style?:
-								| "blockquote"
-								| "h1"
-								| "h2"
-								| "h3"
-								| "h4"
-								| "h5"
-								| "h6"
-								| "normal";
-							listItem?: "bullet" | "number";
-							markDefs?: Array<{
-								href?: string;
-								_type: "link";
-								_key: string;
-							}>;
-							level?: number;
-							_type: "block";
-							_key: string;
-						}>;
-						rightContent?: Array<{
-							children?: Array<{
-								marks?: Array<string>;
-								text?: string;
-								_type: "span";
-								_key: string;
-							}>;
-							style?:
-								| "blockquote"
-								| "h1"
-								| "h2"
-								| "h3"
-								| "h4"
-								| "h5"
-								| "h6"
-								| "normal";
-							listItem?: "bullet" | "number";
-							markDefs?: Array<{
-								href?: string;
-								_type: "link";
-								_key: string;
-							}>;
-							level?: number;
-							_type: "block";
-							_key: string;
-						}>;
-						image?: {
-							asset?: {
-								_ref: string;
-								_type: "reference";
-								_weak?: boolean;
-								[internalGroqTypeReferenceTo]?: "sanity.imageAsset";
-							};
-							hotspot?: SanityImageHotspot;
-							crop?: SanityImageCrop;
-							_type: "image";
-						};
-						imagePosition?: "left" | "right";
+						description?: string;
+						buttonText?: string;
+						link?: string;
 				  }
 			> | null;
 	  }
@@ -1042,7 +881,7 @@ export type GLOBAL_SETTINGS_QUERYResult = {
 import "@sanity/client";
 declare module "@sanity/client" {
 	interface SanityQueries {
-		'\n\t*[_type == "page" || _type == "case"] {\n\t\t_type,\n\t\ttitle,\n\t\tslug,\n\t\tproject,\n\t\tstartDate,\n\t\tendDate,\n\t\theroImage,\n\t\tmodules[] {\n\t\t\t_type == "atAGlanceModule" => {\n\t\t\t\t...,\n\t\t\t},\n\t\t\t_type == "articleTextModule" => {\n\t\t\t\t...,\n\t\t\t},\n\t\t\t_type == "articleQuoteModule" => {\n\t\t\t\t...,\n\t\t\t},\n\t\t\t_type == "articleStatsModule" => {\n\t\t\t\t...,\n\t\t\t},\n\t\t\t_type == "articleGalleryModule" => {\n\t\t\t\t...,\n\t\t\t},\n\t\t\t_type == "articleFigureModule" => {\n\t\t\t\t...,\n\t\t\t},\n\t\t\t_type == "sideBySideModule" => {\n\t\t\t\t...,\n\t\t\t},\n\t\t\t_type == "highlightsModule" => {\n\t\t\t\t...,\n\t\t\t},\n\t\t\t_type == "newsModule" => {\n\t\t\t\t...,\n\t\t\t\t"videoUrl": video.asset -> url,\n\t\t\t},\n\t\t\t_type == "caseHighlightsModule" => {\n\t\t\t\t...,\n\t\t\t\tcases[] -> {\n\t\t\t\t\ttitle,\n\t\t\t\t\tslug,\n\t\t\t\t\tproject,\n\t\t\t\t\tstartDate,\n\t\t\t\t\tendDate,\n\t\t\t\t\theroImage,\n\t\t\t\t},\n\t\t\t},\n\t\t\t_type == "articleHeroModule" => {\n\t\t\t\t...,\n\t\t\t},\n\t\t}\n\t}\n': PAGES_QUERYResult;
+		'\n\t*[_type == "page" || _type == "case"] {\n\t\t_type,\n\t\ttitle,\n\t\tslug,\n\t\tproject,\n\t\tstartDate,\n\t\tendDate,\n\t\theroImage,\n\t\tmodules[] {\n\t\t\t_type == "atAGlanceModule" => {\n\t\t\t\t...,\n\t\t\t},\n\t\t\t_type == "articleTextModule" => {\n\t\t\t\t...,\n\t\t\t},\n\t\t\t_type == "articleQuoteModule" => {\n\t\t\t\t...,\n\t\t\t},\n\t\t\t_type == "articleStatsModule" => {\n\t\t\t\t...,\n\t\t\t},\n\t\t\t_type == "articleGalleryModule" => {\n\t\t\t\t...,\n\t\t\t},\n\t\t\t_type == "articleFigureModule" => {\n\t\t\t\t...,\n\t\t\t},\n\t\t\t_type == "sideBySideModule" => {\n  \t\t\t\t...,\n\t\t\t},\n\t\t\t_type == "highlightsModule" => {\n\t\t\t\t...,\n\t\t\t},\n\t\t\t_type == "newsModule" => {\n\t\t\t\t...,\n\t\t\t\t"videoUrl": video.asset -> url,\n\t\t\t},\n\t\t\t_type == "caseHighlightsModule" => {\n\t\t\t\t...,\n\t\t\t\tcases[] -> {\n\t\t\t\t\ttitle,\n\t\t\t\t\tslug,\n\t\t\t\t\tproject,\n\t\t\t\t\tstartDate,\n\t\t\t\t\tendDate,\n\t\t\t\t\theroImage,\n\t\t\t\t},\n\t\t\t},\n\t\t\t_type == "articleHeroModule" => {\n\t\t\t\t...,\n\t\t\t},\n\t\t}\n\t}\n': PAGES_QUERYResult;
 		'\n\t*[_type == "globalSettings"][0] {\n\t\tglobalTitle,\n\t\tlogo,\n\t\thomePage->,\n\t}\n': GLOBAL_SETTINGS_QUERYResult;
 	}
 }
--- apps/web/src/components/solid/carousel.tsx
+++ apps/web/src/components/solid/carousel.tsx
@@ -1,31 +1,55 @@
 import type { ModuleProps } from "@/scripts/types";
 import createEmblaCarousel from "embla-carousel-solid";
-import { onMount, type ParentComponent } from "solid-js";
+import type { ParentComponent } from "solid-js";
 
 export const Carousel: ParentComponent<ModuleProps<"articleGalleryModule">> = (
 	props,
 ) => {
 	const [emblaRef, emblaApi] = createEmblaCarousel(() => ({
 		slides: ".embla__slide",
+		startIndex: 1,
+		// loop: true,
 	}));
 
-	// onMount(() => {
-	// 	const api = emblaApi();
-	// 	if (api) {
-	// 		console.log(api.slideNodes()); // Access API
-	// 	}
-	// });
+	const previousSlide = () => {
+		const api = emblaApi();
+		if (!api) return;
+
+		api.scrollPrev();
+	};
+
+	const nextSlide = () => {
+		const api = emblaApi();
+		if (!api) return;
+
+		api.scrollNext();
+	};
 
 	return (
-		<div class="embla" ref={emblaRef}>
-			<div className="embla__viewport">
-				<div class="embla__container">{props.children}</div>
+		<>
+			<div class="embla overflow-hidden site-grid" ref={emblaRef}>
+				<div class="embla__viewport col-span-full md:col-span-6 md:col-start-4 ">
+					<div class="embla__container flex gap-4">{props.children}</div>
+				</div>
 			</div>
-
-			<div class="px-[1.5rem] md:px-[3rem]">
-				<button class="embla__prev">Prev</button>
-				<button class="embla__next">Next</button>
+			<div class="site-grid">
+				<div class="mt-[2rem] col-span-full">
+					<button
+						type="button"
+						class="rounded-l-lg p-[0.875rem] bg-[hsla(0,0%,0%,0.05)]"
+						onClick={previousSlide}
+					>
+						Prev
+					</button>
+					<button
+						type="button"
+						class="rounded-r-lg p-[0.875rem] bg-[hsla(0,0%,0%,0.05)]"
+						onClick={nextSlide}
+					>
+						Next
+					</button>
+				</div>
 			</div>
-		</div>
+		</>
 	);
 };
